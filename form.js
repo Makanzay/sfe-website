6328de569f90f9c102e39ac4baeb1b7eabd07904
Makanzay <[andrew.ludo@gmail.com]>
2024-09-08 03:45:31 +0200

    MVP for sfe website:
    just did the main page with the basic caraterisitque and then just add other page and the php side to send mail to the correct mailing box

---
+++ form.js
@@ -0,0 +1,17 @@
+// Contact Form Submission
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for reaching out! We will contact you soon.');
+});
+
+// Quote Form Submission
+document.getElementById('quoteForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Your request has been submitted. We will send you a quote shortly.');
+});
+
+// Job Application Form Submission
+document.getElementById('jobForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for applying! We will review your application.');
+});
